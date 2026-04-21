edf81c486d11d7d4ec268be76dfb82d801fd1aee
tabbychege <tabbywam98@gmail.com>
2026-04-21 21:34:09 +0300

    updated code

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-
+require("datejs");
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  for(let arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+  combinedObject.merge_date = Date.today().toString("M/dd/yyyy");
+  return combinedObject;
+}
 
 
 module.exports = {
